72a8d941f2405b2b3792a17955d86bd57edb5b6c
indigo747 <atamiser@gmail.com>
2010-04-09 10:28:35 -0700

    Added lots of nice comments

--- lib/exec.rb
+++ lib/exec.rb
@@ -14,9 +14,26 @@
 #       MA 02110-1301, USA.
 
 module Rorth
+	
+	# Interprets rorth code. Expects an array. Needs some cleaning up.
+	#
+	# 	code = "2 2 +".split 		# => ['2', '2', '+']
+	# 	exec code # => Depends on input
+	#
+	# An interesting feature/bug is that once a piece of code has been 
+	# appraised for one thing, something that doesn't jump around (ifs, 
+	# words, and loops,) you could theoretorically have a piece of code
+	# do 2 things. For instance, if you had a word called ""word"" (Yes, 
+	# that is "word" surrounded by quotations marks) you could push 
+	# "word" to the stack and execute a command called "word"
 	def exec code
+		# Initialize a point to the first item in the list. We don't use code.each because we want the ability to move dynamicaly through the list instead of stepping through it. This becomes very useful when the jump forward through the code with words, ifs, and whiles.
 		loc = 0
+		
+		# While there is still array to work on...
 		while loc < code.length
+			
+			# Check if the code starts a word definition, if it does, initialize another counter, and read the word into another variable.
 			if code[loc] == ':'
 				word = []
 				word_loc = loc
@@ -26,13 +43,16 @@ module Rorth
 					word_loc += 1
 				end
 				
-				word.shift
-				name = word.shift
-				$words[name] = word
+				word.shift # Remove the :
+				name = word.shift # Remove the name and set it equal to an eponymous variable
+				$words[name] = word # Register the word with the words hash/database
 				loc = word_loc + 1
 			end
 			
+			# I was messing around with several different types of ifs, and this will enable the ifs system with no "else"
 			if $ENABLE_SEMI_IF
+				
+				# This code is very similar to the word definition code...
 				if code[loc].downcase == "if"
 					if_loc = loc
 					if_code = []
@@ -42,6 +62,7 @@ module Rorth
 						if_loc += 1
 					end
 					
+					# Except here, where code is executed if the current stack item is true.
 					if_code.shift
 					if $stack[0]
 						exec if_code
@@ -51,6 +72,7 @@ module Rorth
 				end
 			end
 			
+			# Failed full if section. Not sure why it failed... Maybe we need to regex it to see if it includes an else?
 			if $INSERT_IFS
 				if code[loc].downcase == "if"
 					if_loc = loc
@@ -71,16 +93,20 @@ module Rorth
 				end
 			end
 			
+			# If the code is a primary, convert it, then move it to the front of the stack.
 			if primary? code[loc]
 				$stack = $stack.unshift(convert_primary(code[loc]))
 			end
 			
+			# Check the various places words are stored to see if any of them match the current code. Note that user and header defined words have precidence over built in words. Theoretoricaly, this means you could really mess with the system if you overrode the "+" word, for instance.
 			if $words.has_key? code[loc]
-				forth_exec $words[code[loc]]
+				exec $words[code[loc]]
 			elsif $builtin_words.respond_to? code[loc].downcase.to_sym
 				$builtin_words.send code[loc].downcase.to_sym, $stack
 			end
 			#raise Exception.new "Something went wrong! One of the things you typed in didn\'t get parsed as anything. OH NOES! Word was #{code[loc]}"
+			
+			# Always move forward. This might be moved elsewhere.
 			loc += 1
 			end
 	
